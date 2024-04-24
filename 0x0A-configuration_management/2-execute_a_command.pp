08e16f2276fee55369290bf58b74ded12358bf8f
KenzaDahhoum <kenza.dahhoum@gmail.com>
2024-04-23 17:45:11 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,12 +1,6 @@
-# Define the Puppet manifest to kill the process
-class kill_process {
-    exec { 'killmenow':
-        command     => '/usr/bin/pkill -f killmenow',
-        path        => '/usr/bin',
-        refreshonly => true,
-    }
+# kill process killmenow
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
-
-# Apply the class
-include kill_process
-
